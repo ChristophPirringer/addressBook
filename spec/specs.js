35d13fea1cf348fb772946590dfacca182a1cafe
Lisa Duschek & Christoph Pirringer <student@epicodus.com>
2015-09-14 10:01:24 -0700

    create the address-book app as in the Epicodus-lesson

---
+++ spec/specs.js
@@ -0,0 +1,7 @@
+describe('FindAndReplace', function() {
+
+  // it("replaces the word 'hello' with the word 'world' when only 'hello' is entered", function() {
+  //  expect(FindAndReplace("hello", "hello", "world")).to.equal("world");
+  // });
+
+});
